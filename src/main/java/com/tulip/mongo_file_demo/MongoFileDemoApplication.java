9d854032f95578df6103e4b4d28294105a9b84d1
rocky <tuliprockywithedelwiss@gmail.com>
2018-01-15 02:55:34 +0800

    init project

---
+++ src/main/java/com/tulip/mongo_file_demo/MongoFileDemoApplication.java
@@ -0,0 +1,12 @@
+package com.tulip.mongo_file_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoFileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoFileDemoApplication.class, args);
+	}
+}
